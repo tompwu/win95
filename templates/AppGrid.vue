37576a664ee3a1b1dbc3db9cf6db2eefb8988892
Tom <thomas.ulman@gmail.com>
2025-03-03 21:44:56 +1100

    mobile and tablet optmisations

--- templates/AppGrid.vue
+++ templates/AppGrid.vue
@@ -4,8 +4,6 @@ import { onMounted } from 'vue';
 
 const windowsStore = useWindowsStore()
 
-const gridHeight = ref("")
-
 const openWindow = (windowId) => {
     const payload = {
         windowState: "open",
@@ -18,22 +16,16 @@ const openGithub = () => {
     window.open("https://github.com/DonChiaQE");
 }
 
-
 const getImagePath = (iconImage) => {
     const path = `../assets/win95Icons/${iconImage}`;
     const modules = import.meta.glob("../assets/win95Icons/*", { eager: true });
     const mod = modules[path]
     return mod.default;
 };
-
-onMounted(() => {
-    let gridH = windowsStore.getFullscreenWindowHeight
-    gridHeight.value = gridH.substring(0, gridH.length - 2) - 60 + "px"
-})
 </script>
 
 <template>
-    <nav class="grid-container" :style="{ height: gridHeight }">
+    <nav class="grid-container">
     <li v-for="window in windowsStore.windows" :key="window.key">
       <button
         class="icon"
